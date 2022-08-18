07b0341fd67b22548b9ae9aa26090a07979cf56d
Kevin <jung.kevinj@gmail.com>
2022-08-18 12:19:57 -0700

    Updated App.js and index.js for starting project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import MobileBackground from './components/MobileBackground/MobileBackground';
+import CardForm from './components/CardForm/CardForm';
+import FormSubmit from './components/FormSubmitPage/FormSubmit';
+import {useState} from 'react'
+
+  // <footer>
+  //   Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+  //   Coded by <a href="https://github.com/Kev-Jung">Kevin Jung</a>.
+  // </footer>
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
     </div>
   );
 }
